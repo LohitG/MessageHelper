73033edd29ea3365ce36fa5552d55b9bdf185976
Lohit Geddam <Lohit@Lohits-MacBook-Pro.local>
2019-03-11 10:12:59 -0700

    adding help pages

--- MessageHelper/screens/HomeScreen.js
+++ MessageHelper/screens/HomeScreen.js
@@ -108,7 +108,7 @@ export default class HomeScreen extends React.Component {
 
   _handleHelpPress = () => {
     WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+      'https://docs.google.com/document/d/1OZVqHYkyu_sHYJTiGhRvqB4RIFRJb8CF3zP_Fsw_-dE/edit'
     );
   };
 }
